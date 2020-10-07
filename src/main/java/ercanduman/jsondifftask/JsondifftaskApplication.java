688998df82134299e05ad4f33f5ec41c261c83b2
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-10-07 12:33:17 +0300

    initial commit

---
+++ src/main/java/ercanduman/jsondifftask/JsondifftaskApplication.java
@@ -0,0 +1,11 @@
+package ercanduman.jsondifftask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsondifftaskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JsondifftaskApplication.class, args);
+    }
+}
---
+++ src/test/java/ercanduman/jsondifftask/JsondifftaskApplicationTests.java
@@ -0,0 +1,11 @@
+package ercanduman.jsondifftask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsondifftaskApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
