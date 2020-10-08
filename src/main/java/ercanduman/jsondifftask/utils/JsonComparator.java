813fc02503b0324fef50b8122f07ea4fa04c82aa
Ercan Duman✔️ <ercanduman30@gmail.com>
2020-10-08 12:15:14 +0300

    JsonComparator is finished and object comments added

--- src/main/java/ercanduman/jsondifftask/utils/JsonComparator.java
+++ src/main/java/ercanduman/jsondifftask/utils/JsonComparator.java
@@ -4,44 +4,59 @@ import ercanduman.jsondifftask.data.entity.JsonObject;
 
 import java.util.Arrays;
 
+/**
+ * Compares two json string
+ */
 public class JsonComparator {
 
-    // if both objects are equal, then return "Objects are equal"
-    // If their sizes are different, then return "Different size"
-    // If sizes are equal then return different offsets length of difference 
-
+    /**
+     * Compares two JSON strings and returns results based on equality of data
+     * <p>
+     * if two JSON data are equal return that
+     * if Json objects are different but have same length, returns that
+     * if Objects' sizes are equal then return different offset and length of difference
+     *
+     * @param firstObject  first {@link JsonObject}
+     * @param secondObject second {@link JsonObject}
+     * @return result text
+     */
     public static String compare(JsonObject firstObject, JsonObject secondObject) {
         if (firstObject == null && secondObject == null) return "Both objects are NULL";
 
         if (firstObject == null || secondObject == null) return "NULL comparison not allowed";
 
-        String first = firstObject.getContent();
-        String second = secondObject.getContent();
-        char[] firstChars = first.toCharArray();
-        char[] secondChars = second.toCharArray();
+        char[] firstChars = removeAllWhiteSpaces(firstObject.getContent()).toCharArray();
+        char[] secondChars = removeAllWhiteSpaces(secondObject.getContent()).toCharArray();
 
         String result;
-        if (first.equals(second)) {
-            result = "Objects are equal - \n" + first + "\n" + second;
-        } else if (first.length() != second.length()) {
+        if (Arrays.equals(firstChars, secondChars)) {
+            result = "Objects are equal - \n" + firstObject.getContent() + "\n" + secondObject.getContent();
+        } else if (firstChars.length != secondChars.length) {
             result = "Objects have different sizes";
         } else {
-            int length = first.length();
 
-            char[] differences = new char[length];
-
+            // This array created in order to display differences text
+            char[] differences = new char[firstChars.length];
             int offset = -1;
-            for (int i = 0; i < length; i++) {
+            for (int i = 0; i < firstChars.length; i++) {
                 if (firstChars[i] != secondChars[i]) {
                     differences[i] = firstChars[i];
-
                     if (offset < 0) offset = i;
                 }
             }
             result = "Objects have same size b ut has differences starts at offset: " + offset
-                    + " \nDifferences are:" + Arrays.toString(differences)
-                    + " \nDiff length: " + differences.length;
+                    + " \nDifference length :" + differences.length;
         }
         return result;
+    }
+
+    /**
+     * Removes all white spaces from input text
+     *
+     * @param input text to remove spaces from.
+     * @return returns pure string with text
+     */
+    private static String removeAllWhiteSpaces(String input) {
+        return input.replaceAll("\\s+", "");
     }
 }
